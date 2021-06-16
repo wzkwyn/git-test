424a1ae6cc9145718717a68c78927aab50bbd049
wzk <1114682049@qq.com>
2021-06-16 14:57:51 +0800

    添加Git222

--- src/main/java/com/wzk/gittest/Git2.java
+++ src/main/java/com/wzk/gittest/Git2.java
@@ -8,5 +8,6 @@ public class Git2 {
     public static void main(String[] args) {
         System.out.println("Git2dev");
         System.out.println("Git2dev");
+        System.out.println("Git2dev");
     }
 }
--- src/main/java/com/wzk/gittest/GitTest.java
+++ src/main/java/com/wzk/gittest/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Hello, Git!");
         System.out.println("Hello, dev!");
         System.out.println("Hello, dev!");
+        System.out.println("Hello, dev!");
     }
 }
