1538860237c332b71748b045a862c49ac2a878d8
wzk <1114682049@qq.com>
2021-06-16 10:35:43 +0800

    初始化项目

---
+++ src/main/java/com/wzk/gittest/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzk.gittest;
+
+/**
+ * @author wzk
+ * @create 2021/6/16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
