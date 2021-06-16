5a0df37899928ea7fc9923b141be9aa574ea5426
wzk <1114682049@qq.com>
2021-06-16 14:02:36 +0800

    第一行修改为hello dev

--- src/main/java/com/wzk/gittest/GitTest.java
+++ src/main/java/com/wzk/gittest/GitTest.java
@@ -6,7 +6,7 @@ package com.wzk.gittest;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, dev!");
         System.out.println("Hello, Git!");
         System.out.println("Hello, dev!");
     }
