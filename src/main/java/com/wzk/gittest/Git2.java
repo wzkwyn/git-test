f6f0bbbcfac3b166af6f1bb45ce9a836d421c89d
wzk <1114682049@qq.com>
2021-06-16 14:39:09 +0800

    添加Git2

---
+++ src/main/java/com/wzk/gittest/Git2.java
@@ -0,0 +1,11 @@
+package com.wzk.gittest;
+
+/**
+ * @author wzk
+ * @create 2021/6/16
+ */
+public class Git2 {
+    public static void main(String[] args) {
+        System.out.println("Git2");
+    }
+}
