7b9697676eba1e4a5c56d73e9b7c5bef52434b4c
wzk <1114682049@qq.com>
2021-06-16 15:07:23 +0800

    添加Git2222

--- src/main/java/com/wzk/gittest/GitTest.java
+++ src/main/java/com/wzk/gittest/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello, dev!");
         System.out.println("Hello, dev!");
         System.out.println("Hello, dev!");
+        System.out.println("Hello, dev!");
     }
 }
